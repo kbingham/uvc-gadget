b28524b5eff7d61670a2f1794ea316dc66392315
Laurent Pinchart <laurent.pinchart@ideasonboard.com>
2018-05-21 21:43:21 +0300

    uvc-gadget: Use macros from tools.h
    The tools.h header defines clamp and ARRAY_SIZE macros. Use the header
    to replace the private implementation.
    
    Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>

--- uvc-gadget.c
+++ uvc-gadget.c
@@ -39,20 +39,10 @@
 #include <linux/videodev2.h>
 
 #include "events.h"
+#include "tools.h"
 
 #define UVC_INTF_CONTROL	0
 #define UVC_INTF_STREAMING	1
-
-#define clamp(val, min, max) ({                 \
-        typeof(val) __val = (val);              \
-        typeof(min) __min = (min);              \
-        typeof(max) __max = (max);              \
-        (void) (&__val == &__min);              \
-        (void) (&__val == &__max);              \
-        __val = __val < __min ? __min: __val;   \
-        __val > __max ? __max: __val; })
-
-#define ARRAY_SIZE(a)	((sizeof(a) / sizeof(a[0])))
 
 struct uvc_device
 {
