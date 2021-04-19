9a04e7332896e912326cb4efd1b4ce49be261eff
Caleb Connolly <caleb.connolly@ideasonboard.com>
2021-04-19 02:11:57 +0100

    uvc.c: start refactoring to better represent specification

--- lib/uvc.c
+++ lib/uvc.c
@@ -25,6 +25,11 @@
 #include "v4l2.h"
 #include "log.h"
 
+#define PU_BRIGHTNESS_MIN_VAL		0
+#define PU_BRIGHTNESS_MAX_VAL		255
+#define PU_BRIGHTNESS_STEP_SIZE		1
+#define PU_BRIGHTNESS_DEFAULT_VAL	127
+
 struct uvc_device
 {
 	struct v4l2_device *vdev;
@@ -41,6 +46,8 @@ struct uvc_device
 	unsigned int width;
 	unsigned int height;
 	unsigned int maxsize;
+
+	int brightness;
 };
 
 struct uvc_device *uvc_open(const char *devname, struct uvc_stream *stream)
@@ -139,27 +146,114 @@ uvc_events_process_standard(struct uvc_device *dev,
 	(void)resp;
 }
 
-// Handles processing unit controls ?
+// Processing unit brightness control
 static void
-uvc_events_process_control(struct uvc_device *dev, uint8_t req, uint8_t cs,
-			   struct uvc_request_data *resp)
+uvc_events_pu_brightness_control(struct uvc_device *dev, uint8_t req,
+				struct uvc_request_data *resp)
 {
-	log_warn("control request (req 0x%02x cs 0x%02x)", req, cs);
-
 	switch (req) {
 		case UVC_GET_INFO:
 			resp->data[0] = 0x03;
 			resp->length = 1;
 			break;
 		case UVC_SET_CUR:
-			log_debug("Control SET_CUR");
+			resp->data[0] = 0x0;
+			resp->length = 1;
 			break;
+		case UVC_GET_DEF:
+			resp->data[0] = PU_BRIGHTNESS_DEFAULT_VAL;
+			resp->length = 2;
+			break;
+		case UVC_GET_RES:
+			resp->data[0] = PU_BRIGHTNESS_STEP_SIZE;
+			resp->length = 2;
+			break;
+		case UVC_GET_MIN:
+			resp->data[0] = PU_BRIGHTNESS_MIN_VAL;
+			resp->length = 2;
+			break;
+		case UVC_GET_MAX:
+			resp->data[0] = PU_BRIGHTNESS_MAX_VAL;
+			resp->length = 2;
+			break;
+		case UVC_GET_CUR:
+			resp->length = 2;
+			memcpy(&resp->data[0], &dev->brightness,
+					resp->length);
 	}
 }
 
-// Handles video streaming controls
+// Default values for an unimplemented control, this function
+// will only be hit if the f_uvc driver gets updated to expose
+// more than just the controls currently handled in
+// uvc_events_setup_pu_control().
 static void
-uvc_events_process_streaming(struct uvc_device *dev, uint8_t req, uint8_t cs,
+uvc_events_pu_unimplemented_control(struct uvc_device *dev, uint8_t req,
+				struct uvc_request_data *resp)
+{
+	/*
+	* We don't support this control, so STALL the
+	* default control endpoint.
+	*/
+	resp->length = -EL2HLT;
+	// switch (req) {
+	// 	case UVC_GET_INFO:
+	// 		// Report that we support GET, SET
+	// 		// and that this control is disabled
+	// 		// ("under device control");
+	// 		resp->data[0] = 0x07;
+	// 		resp->length = 1;
+	// 		break;
+	// 	case UVC_SET_CUR:
+	// 		log_debug("Unimplemented control SET_CUR");
+	// 		break;
+	// 	case UVC_GET_DEF:
+	// 		log_debug("Unimplmented control GET_DEF");
+	// 		resp->data[0] = 127;
+	// 		resp->length = 2;
+	// 		break;
+	// 	case UVC_GET_
+	// }
+}
+
+// Handle processing unit controls
+static void
+uvc_events_setup_pu_control(struct uvc_device *dev, uint8_t req, uint8_t cs,
+			   struct uvc_request_data *resp)
+{
+	log_debug("control request (req 0x%02x cs 0x%02x)", req, cs);
+
+	// The f_uvc driver is hard coded to only support a brightness
+	// control, but that should change in the future.
+	switch (cs) {
+		case UVC_PU_BRIGHTNESS_CONTROL:
+			uvc_events_pu_brightness_control(dev, req, resp);
+			break;
+		default:
+			log_warn("Unimplemented control, making best guess");
+			uvc_events_pu_unimplemented_control(dev, req, resp);
+			break;
+	}
+
+	// switch (req) {
+	// 	case UVC_GET_INFO:
+	// 		resp->data[0] = 0x03;
+	// 		resp->length = 1;
+	// 		break;
+	// 	case UVC_SET_CUR:
+	// 		log_debug("Control SET_CUR");
+	// 		break;
+	// 	case UVC_GET_DEF:
+	// 		log_debug("Control GET_DEF");
+	// 		resp->data[0] = PU_BRIGHTNESS_DEFAULT_VAL;
+	// 		resp->length = 2;
+	// 		break;
+	// }
+}
+
+// Handle video streaming controls
+static void
+uvc_events_setup_vs_control(struct uvc_device *dev, uint8_t req, uint8_t cs,
 			     struct uvc_request_data *resp)
 {
 	struct uvc_streaming_control *ctrl;
@@ -211,57 +305,71 @@ uvc_events_process_class(struct uvc_device *dev,
 			 const struct usb_ctrlrequest *ctrl,
 			 struct uvc_request_data *resp)
 {
-	unsigned int interface = ctrl->wIndex & 0xff;
-	// control selector
+	// Id of the entity (unit) we're handling
+	unsigned int entity_id = ctrl->wIndex & 0xff;
+	// Control selector (which control to handle)
 	uint8_t cs =ctrl->wValue >> 8;
 
 	if ((ctrl->bRequestType & USB_RECIP_MASK) != USB_RECIP_INTERFACE)
 		return;
 
-	if (interface == dev->fc->control.intf.bInterfaceNumber)
-		uvc_events_process_control(dev, ctrl->bRequest, cs, resp);
-	else if (interface == dev->fc->streaming.intf.bInterfaceNumber)
-		uvc_events_process_streaming(dev, ctrl->bRequest, cs, resp);
+	// This is the Processing Unit control
+	if (entity_id == dev->fc->control.intf.bInterfaceNumber)
+		uvc_events_setup_pu_control(dev, ctrl->bRequest, cs, resp);
+	// This is the VideoStreaming control
+	else if (entity_id == dev->fc->streaming.intf.bInterfaceNumber)
+		uvc_events_setup_vs_control(dev, ctrl->bRequest, cs, resp);
 }
 
 static void
-uvc_events_process_control_get(struct uvc_device *dev,
+uvc_events_process_setup(struct uvc_device *dev,
 			 const struct usb_ctrlrequest *ctrl,
 			 struct uvc_request_data *resp)
 {
-	dev->control = 0;
-
 	dump_usb_ctrlrequest(ctrl);
 
 	switch (ctrl->bRequestType & USB_TYPE_MASK) {
-	case USB_TYPE_STANDARD:
-		uvc_events_process_standard(dev, ctrl, resp);
-		break;
+	// Do we EVER hit this case????
+		case USB_TYPE_STANDARD:
+			uvc_events_process_standard(dev, ctrl, resp);
+			break;
 
-	case USB_TYPE_CLASS:
-		uvc_events_process_class(dev, ctrl, resp);
-		break;
+		case USB_TYPE_CLASS:
+			uvc_events_process_class(dev, ctrl, resp);
+			break;
 
-	default:
-		break;
+		default:
+			break;
 	}
 }
 
 static void
-uvc_events_process_control_set(struct uvc_device *dev,
+uvc_events_process_data_pu_control(struct uvc_device *dev, uint8_t cs,
+			const struct uvc_request_data *data,
+			struct uvc_request_data *resp)
+{
+	switch (cs)
+	{
+		case UVC_PU_BRIGHTNESS_CONTROL:
+			memcpy(&dev->brightness, data->data, data->length);
+			log_debug("Set brightness value: %d", dev->brightness);
+			break;
+		
+		default:
+			break;
+	}
+}
+
+static void
+uvc_events_process_data_vs_control(struct uvc_device *dev, uint8_t cs,
 			const struct uvc_request_data *data,
 			struct uvc_request_data *resp)
 {
 	const struct uvc_streaming_control *ctrl =
 		(const struct uvc_streaming_control *)&data->data;
 	struct uvc_streaming_control *target;
-	const struct usb_ctrlrequest *usb_ctrl = &data->setup;
 
-	dump_usb_ctrlrequest(usb_ctrl);
-
-	dev->control = usb_ctrl->wValue >> 8;
-
-	switch (dev->control) {
+	switch (cs) {
 	case UVC_VS_PROBE_CONTROL:
 		log_debug("setting probe control, length = %d", data->length);
 		target = &dev->probe;
@@ -281,7 +389,7 @@ uvc_events_process_control_set(struct uvc_device *dev,
 	uvc_fill_streaming_control(dev, target, ctrl->bFormatIndex,
 				   ctrl->bFrameIndex, ctrl->dwFrameInterval);
 
-	if (dev->control == UVC_VS_COMMIT_CONTROL) {
+	if (cs == UVC_VS_COMMIT_CONTROL) {
 		const struct uvc_function_config_format *format;
 		const struct uvc_function_config_frame *frame;
 		struct v4l2_pix_format pixfmt;
@@ -308,7 +416,28 @@ uvc_events_process_control_set(struct uvc_device *dev,
 		fps = 1.0 / (target->dwFrameInterval / 10000000.0);
 		uvc_stream_set_frame_rate(dev->stream, fps);
 	}
+}
 
+static void
+uvc_events_process_data(struct uvc_device *dev,
+			const struct uvc_request_data *data,
+			struct uvc_request_data *resp)
+{
+	
+	const struct usb_ctrlrequest *ctrl = &data->setup;
+
+	dump_usb_ctrlrequest(ctrl);
+
+	unsigned int entity_id = ctrl->wIndex & 0xff;
+	uint8_t cs = ctrl->wValue >> 8;
+
+	// This is the Processing Unit control
+	if (entity_id == dev->fc->control.intf.bInterfaceNumber)
+		uvc_events_process_data_pu_control(dev, cs, data, resp);
+	// This is the VideoStreaming control
+	else if (entity_id == dev->fc->streaming.intf.bInterfaceNumber)
+		uvc_events_process_data_vs_control(dev, cs, data, resp);
+	
 	resp->length = 0;
 }
 
@@ -328,7 +457,7 @@ static void uvc_events_process(void *d)
 	}
 	
 	if (uvc_event->data.length == 26 && uvc_event->data.setup.wLength == 0) {
-		log_error("Detected an empty (null) packet\n");
+		log_error("Detected an empty (null) packet");
 		return;
 	}
 
@@ -341,18 +470,20 @@ static void uvc_events_process(void *d)
 		return;
 
 	case UVC_EVENT_SETUP:
-		uvc_events_process_control_get(dev, &uvc_event->req, &resp);
+		uvc_events_process_setup(dev, &uvc_event->req, &resp);
 		break;
 
 	case UVC_EVENT_DATA:
-		uvc_events_process_control_set(dev, &uvc_event->data, &resp);
+		uvc_events_process_data(dev, &uvc_event->data, &resp);
 		break;
 
 	case UVC_EVENT_STREAMON:
+		log_debug("Enabling UVC stream");
 		uvc_stream_enable(dev->stream, 1);
 		return;
 
 	case UVC_EVENT_STREAMOFF:
+		log_debug("Disabling UVC stream");
 		uvc_stream_enable(dev->stream, 0);
 		return;
 	}
